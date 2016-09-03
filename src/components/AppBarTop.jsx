05ebdbb2cd0af305f5b4013a9ba26a6a85f6f8f0
David Ffrench <davidffrench@gmail.com>
2016-09-03 17:36:10 +0100

    Initial search bar for grid list

--- src/components/AppBarTop.jsx
+++ src/components/AppBarTop.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, { Component } from 'react';
 import AppBar from 'material-ui/AppBar';
 import IconButton from 'material-ui/IconButton';
 import NavigationClose from 'material-ui/svg-icons/navigation/close';
 import FlatButton from 'material-ui/FlatButton';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AutoComplete from 'material-ui/AutoComplete';
 
 function handleTouchTap() {
   alert('onTouchTap triggered on the title component');
@@ -13,17 +14,39 @@ const styles = {
   title: {
     cursor: 'pointer',
   },
+  searchTextField: {
+    backgroundColor: 'transparent',
+    'input > color': 'white',
+  },
 };
 
-const AppBarTop = () => (
-  <MuiThemeProvider>
-    <AppBar
-      title={<span style={styles.title}>User List</span>}
-      onTitleTouchTap={handleTouchTap}
-      // iconElementLeft={<IconButton><NavigationClose /></IconButton>}
-      // iconElementRight={<FlatButton label="Save" />}
-    />
-  </MuiThemeProvider>
-);
+class AppBarTop extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      dataSource: ['david', 'Samantha'],
+    };
+  }
+
+  render() {
+    return (
+      <MuiThemeProvider>
+        <AppBar
+          title={<span style={styles.title}>User List</span>}
+          onTitleTouchTap={handleTouchTap}
+          // iconElementLeft={<IconButton><NavigationClose /></IconButton>}
+          iconElementRight={
+            <AutoComplete
+              hintText="Search by Name"
+              dataSource={this.state.dataSource}
+              textFieldStyle={styles.searchTextField}
+            />
+          }
+        />
+      </MuiThemeProvider>
+    );
+  }
+}
 
 export default AppBarTop;
